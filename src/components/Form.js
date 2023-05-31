90ee9241a6986f541c5abbf81023a7bfdc0a566d
lauracaurin <laura_cd1@hotmail.com>
2023-05-31 16:57:48 +0200

    Publish in GitHub Pages

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,16 +2,27 @@ const Form = ({ handleOnChange, valueInput }) => {
   const handleInputOnChange = (ev) => {
     handleOnChange(ev.target.value)
   }
+
+  const handleKeyDown = (ev) => {
+    ev.target.setSelectionRange(0, 1);
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <label className="title" htmlFor="last-letter">Escribe una letra:</label>
       <input
-        autocomplete="off"
+        autoFocus
+        autoComplete="off"
         className="form__input"
         maxlength="1"
         type="text"
         name="last-letter"
         id="last-letter"
+        onKeyDown={handleKeyDown}
         onChange={handleInputOnChange}
         value={valueInput}
       />
